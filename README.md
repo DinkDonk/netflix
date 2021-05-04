700ede343ee2a26e51b2b55ffe9a712907c81a3e
Rune Warhuus <rune@dinkdonk.no>
2021-05-04 20:38:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Netflix MacOS App
+=================
+
+A crude native MacOS application wrapper for Netflix.  
+Made so that I can start Netflix via Spotlight, and navigate to a dedicated app using `⌘ + TAB`.  
+
+![screenshot](design/screenshot.png)
--- netflix/ViewController.swift
+++ netflix/ViewController.swift
@@ -6,13 +6,44 @@
 //
 
 import Cocoa
+import WebKit
+
+class WebView: WKWebView {
+    override func becomeFirstResponder() -> Bool {
+        return false
+    }
+    
+    override func resignFirstResponder() -> Bool {
+        return true
+    }
+}
 
 class ViewController: NSViewController {
+    private let webView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        let webView = WebView(frame: .zero, configuration: configuration)
+        return webView
+    }()
+    
+    override func becomeFirstResponder() -> Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(webView)
+        guard let url = URL(string: "https://netflix.com/") else {return}
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+    override func viewDidLayout() {
+        super.viewDidLayout()
+        webView.frame = view.bounds
     }
 
     override var representedObject: Any? {
@@ -20,7 +51,4 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
-
