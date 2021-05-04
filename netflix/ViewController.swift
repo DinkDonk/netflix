e382c2909aa8b9700d7eaba78598e546894df824
Rune Warhuus <rune@dinkdonk.no>
2021-05-04 16:46:10 +0200

    Initial Commit

---
+++ netflix/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  netflix
+//
+//  Created by Rune Warhuus on 04/05/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
